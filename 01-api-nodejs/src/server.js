c48c7c22c8c383aac8ae9f68e993e447f7d96be3
artuur <arturcastrodossantos.com@gmail.com>
2024-11-21 21:50:36 -0300

    implementação do db

--- 01-api-nodejs/src/server.js
+++ 01-api-nodejs/src/server.js
@@ -1,28 +1,36 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from '../database/database.js'
 
+const database = new Database()
 
-const users = []
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
 
     const {method, url} = req
 
+    await json(req, res)
+
     if (method === "GET" && url === "/tasks") {
+        const data = database.select('users')
         return res
         .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+        .end(JSON.stringify(data))
     }
+    
     if (method === "POST" && url === "/tasks") {
+        const {name, email} = req.body
         
-        users.push({
+        const users = {
             id: crypto.randomUUID(),
-            name: 'artur'
-        })
+            name,
+            email
+        }
+        database.insert('users', users)
     }
     
     return res
     .setHeader('Content-type','application/json')
-    .writeHead(201).end(JSON.stringify(users))
+    .writeHead(201).end()
 })
 
 server.listen(3000, () => {
